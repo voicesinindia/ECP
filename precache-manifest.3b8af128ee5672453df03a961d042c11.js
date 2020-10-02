e9cc7e993102d20d90242cdf0d0a95ad41ca3692
voicesinindia <72255061+voicesinindia@users.noreply.github.com>
2020-10-02 16:47:20 +0530

    First commit - manual
    God knows why tf I still don't know Git.

---
+++ precache-manifest.3b8af128ee5672453df03a961d042c11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225c767bfe5c759f7765fbf1b6094aad",
+    "url": "/ECP/index.html"
+  },
+  {
+    "revision": "0ad68ef8ba0a1289d20a",
+    "url": "/ECP/static/css/main.f53207ca.chunk.css"
+  },
+  {
+    "revision": "5708c56743a2fefcbf4c",
+    "url": "/ECP/static/js/2.20769ad1.chunk.js"
+  },
+  {
+    "revision": "9a8998d92f10b493b9441fd8e8324abe",
+    "url": "/ECP/static/js/2.20769ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad68ef8ba0a1289d20a",
+    "url": "/ECP/static/js/main.fad2d8ee.chunk.js"
+  },
+  {
+    "revision": "04ba86164cff8e1d41f4",
+    "url": "/ECP/static/js/runtime-main.ae335127.js"
+  },
+  {
+    "revision": "b29a0c73f8f1a0d6199ecb96f08a611a",
+    "url": "/ECP/static/media/MainPicture.b29a0c73.webp"
+  }
+]);
